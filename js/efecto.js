3fa6d0ab910f257e5d778730e469a09ea8980e45
javohotel <ja.ramirez@live.cl>
2017-01-06 16:04:00 -0400

    subiendo los rimeros efectos que pueden servir para cualquier eventualidad, listos para aplicar.con css completo y en algunos casos js, con imagenes de respaldo

---
+++ js/efecto.js
@@ -0,0 +1,13 @@
+
+/*funcion nav*/
+
+$(window).load(function() {
+
+  $(".btn-nav").on("click tap", function() {
+
+    $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+
+    $(this).toggleClass("animated");
+ });
+
+});
